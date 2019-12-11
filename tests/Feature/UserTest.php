fd0d50f77ebd5c8f6c65573b1933b351402b5c56
Fikayo <fik4christ@yahoo.com>
2019-12-12 00:05:44 +0100

    Added tests, edit configuration and .env.testing

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -6,6 +6,9 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
+use App\User;
+use Illuminate\Support\Str;
+
 class UserTest extends TestCase
 {
     /**
@@ -13,24 +16,51 @@ class UserTest extends TestCase
      *
      * @return void
      */
-    public function testExample()
+
+    private $name = "Test User";
+    private $password = "mypassword";
+    
+    public function testUserCreation()
     {
+       
+
         $response = $this->withHeaders([
             'Content-Type' => 'application/json',
             'X-Requested-With' => 'XMLHttpRequest'
         ])->json('POST', '/api/auth/signup', [
-            'name' => 'Sally', 
-            'email' => 'sally3@company.com',
-            'password' => 'sally', 
-            'password_confirmation' => 'sally'
-        ]);
+            'name' => $this->name, 
+            'email' => Str::random(7)."@mail.com",
+            'password' => $this->password, 
+            'password_confirmation' => $this->password
+        ]); 
 
-        //dump($response->getContent()); 
 
         $response
             ->assertStatus(201)
             ->assertExactJson([
                 'message' => "Successfully created user!",
             ]);
-    }
+    }//testUserCreation
+
+    public function testUserLogin()
+    {
+        $email = Str::random(7)."@mail.com";
+
+        $user = new User([
+            'name' => $this->name,
+            'email' => $email,
+            'password' => bcrypt($this->password)
+        ]);        
+        
+        $user->save();     
+        
+        $response = $this->postJson('/api/auth/login', [
+            'email' => $email,
+            'password' => $this->password
+        ]);
+        
+        $response->assertStatus(200);
+    }//testUserLogin
+
+    
 }
